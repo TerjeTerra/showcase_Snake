bbdbad8f80ba0cc4ed58527d0eab0e2c9d85d54a
TerjeTerra <103331211+TerjeTerra@users.noreply.github.com>
2024-02-10 19:57:53 +0100

    New version 4.01
    Improved version, 2 player mode enabled. Fixed bug.

--- food.py
+++ food.py
@@ -1,24 +1,5 @@
-'''
-Notater fra utviklingsprosessen:
-Skal definere klassen food
-    attributter:
-    - shape
-    - color
-    - speed
-    - type eller value # kan kobles til poengsystem
-    - visible # hvor lenge den skal være synlig
-    metoder:
-    - getValue
-    - moveFood # for å flytte maten ut av skjermen 
+# food for snakes...
 
-    KOMMENTAR 8/4-22 Løst problem med at objekter av klassen Food ikke
-    gjenkjennes som faktiske objekter av typen skilpadder (e.l.)
-    Handlet om inheritance. Lærte mer!
-
-    Fortsette med hovedprogram og alle funksjoner i spillet
-    Først: to ulike mat-parametere/beregninger: frekvens og synlighet(varighet)
-    Neste skritt: poeng og to slanger (først brukerstyrt, så datastyrt)
-'''
 import random
 import turtle
 
@@ -30,9 +11,6 @@ class Food(turtle.Turtle):
         self.__visible = 10000 # hvor lenge maten vises hver gang
         self.__hidden = 0 # hvor lenge maten er skjult per gang
         self.__timer = 10000
-
-        #colors = random.choice(['red', 'green'])
-        #shapes = random.choice(['square', 'triangle', 'circle'])
 
         turtle.Turtle.__init__(self) # kaller __init__ av foreldre klassen
         self.penup()
@@ -49,7 +27,7 @@ class Food(turtle.Turtle):
             self.__visible = intervall * 2
             self.__hidden = intervall
 
-        elif type == 3: # svært sjelden
+        elif type == 3: # mest sjelden
             self.shape('turtle')
             self.color('white')
             self.__points = 5
@@ -61,7 +39,7 @@ class Food(turtle.Turtle):
         self.showturtle()
 
         if self.__type != 1: # (type 1 skal ikke skjules automatisk)
-            self.__timer = int(self.__hidden * (0.5 + random.random())) # første forflytting i spillet skjer tilfeldig tid
+            self.__timer = int(self.__hidden * (0.5 + random.random())) # forste forflytting i spillet skjer tilfeldig tid
             
     def get_type(self):
         return self.__type
@@ -80,7 +58,7 @@ class Food(turtle.Turtle):
 
     def reset_timer(self):
         if self.__type != 1: # (type 1 skal ikke skjules automatisk)
-            self.__timer = int(self.__hidden * (0.5 + random.random())) # første forflytting i spillet skjer tilfeldig tid
+            self.__timer = int(self.__hidden * (0.5 + random.random())) # forste forflytting i spillet skjer tilfeldig tid
 
     def get_points(self):
         return self.__points
